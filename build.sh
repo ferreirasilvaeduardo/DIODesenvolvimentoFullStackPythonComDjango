b7f1b9475fb48974de961f0b048f3c61a42c9563
Eduardo da Silva Ferreira <eduardo.s.fereira@gmail.com>
2025-03-27 19:10:56 -0300

    commit

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 # pip install -r requirements.txt
 pip install poetry -U
-poetry install --no-root --no-dev
+poetry install --no-root
 
 # Convert static asset files
 python manage.py collectstatic --no-input
